0bc63e0cff0f595170b4ff81c14be326e15e753f
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2023-03-17 16:05:22 -0300

    feat/delete and update

--- core/views.py
+++ core/views.py
@@ -39,7 +39,13 @@ def event_list(request):
 
 @login_required(login_url='/login/')
 def event(request):
-  return render(request, 'event.html')
+  id_event = request.GET.get('id')
+  datas = {}
+  if id_event:
+    datas['event'] = Event.objects.get(id=id_event)
+  return render(request, 'event.html', datas)
+
+  
 
 @login_required(login_url='/login/')
 def event_submit(resquest):
@@ -48,8 +54,28 @@ def event_submit(resquest):
     event_date = resquest.POST.get("event_date")
     description = resquest.POST.get("description")
     user = resquest.user
-    Event.objects.create(title=title,
-                        event_date=event_date,
-                        description = description,
-                        user=user )
+    id_event = resquest.POST.get("id_event")
+    if id_event:
+      event = Event.objects.get(id=id_event)
+      if event.user == user:
+        event.title = title
+        event.description = description
+        event.event_date = event_date
+        event.save()
+      # Event.objects.filter(id=id_event).update(title=title,
+      #                                          event_date=event_date,
+      #                                          description = description
+      #                                          )
+    else:
+      Event.objects.create(title=title,
+                          event_date=event_date,
+                          description = description,
+                          user=user )
     return redirect('/')
+@login_required(login_url='/login/')
+def event_delete(request, id_event):
+  user = request.user
+  event = Event.objects.get(id=id_event)
+  if user == event.user :
+    event.delete()
+  return redirect('/')
